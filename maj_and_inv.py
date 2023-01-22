14d8b6a064e3eb2247737522e98cb54331894ab5
Yonatan Dahary <yonatan.dahary@gmail.com>
2023-01-22 03:45:52 +0200

    more advancements

--- maj_and_inv.py
+++ maj_and_inv.py
@@ -1,6 +1,6 @@
 import useful_functions as uf
 
-my_set = {1,2,3,4,5}
+my_set = {1,2,3,4,5,6}
 
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"mah",uf.lb,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"mah",uf.rs,len(my_set)))
@@ -8,13 +8,20 @@ my_set = {1,2,3,4,5}
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"mah",uf.rb,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"mah",uf.inv,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"mah",uf.maj,len(my_set)))
+# uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"mah",uf.maj_d,len(my_set)))
+# uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"mah",uf.div_k,len(my_set)))
+# uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"mah",uf.stat_even,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.lb,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.rs,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.ls,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.rb,len(my_set)))
-uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.magic_maj,len(my_set)))
-print("")
-uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.magic_inv,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.inv,len(my_set)))
 # uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.canonical_maj,len(my_set)))
+# uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.magic_maj,len(my_set)))
+# uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.magic_inv,len(my_set)))
+# uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.magic_stat_even,len(my_set)))
+# uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.magic_div_k,len(my_set)))
+# uf.print_dict(uf.get_a_statistic(uf.get_partitions(my_set),"can",uf.magic_maj_d,len(my_set)))
+
+
 
--- useful_functions.py
+++ useful_functions.py
@@ -89,6 +89,18 @@ def maj(permutation):
             major_index += i + 1
     return major_index
 
+def maj_d(permutation,d = 5):
+    major_index_d = 0
+    for i in range(len(permutation)):
+      if(i+d<len(permutation)):
+        if permutation[i] > permutation[i + d]:
+            major_index_d += i + 1
+      for j in range(i + 1, i+d):
+        if j<len(permutation):
+          if permutation[i] > permutation[j]:
+              major_index_d += 1
+    # print(str(permutation)+'\t'+str(major_index_d))
+    return major_index_d
 
 def magic_inv(rep):
     inversion = 0
@@ -97,16 +109,30 @@ def magic_inv(rep):
             if rep[i] < rep[j]:
                 inversion += 1
 
-    print(str(rep)+'\t'+str(inversion))
+    # print(str(rep)+'\t'+str(inversion))
     return inversion
+
 
 def magic_maj(permutation):
     major_index = 0
     for i in range(len(permutation) - 1):
         if permutation[i] < permutation[i + 1]:
             major_index += len(permutation)-(i+1)
-    print(str(permutation)+'\t'+str(major_index))
+    # print(str(permutation)+'\t'+str(major_index))
     return major_index
+
+def magic_maj_d(permutation,d = 2):
+    major_index_d = 0
+    for i in range(len(permutation)):
+      if(i+d<=len(permutation)-1):
+        if permutation[i] < permutation[i + d]:
+            major_index_d += len(permutation)-(i+1)
+      for j in range(i + 1, i+d):
+        if j<len(permutation):
+          if permutation[i] < permutation[j]:
+              major_index_d += 1
+    #print(str(permutation)+'\t'+str(major_index_d))
+    return major_index_d
 
 def canonical_maj(permutation):
     major_index = 0
@@ -134,3 +160,52 @@ def get_a_statistic(partitions,rep_type:str,statistic,length:int):
 
 def print_dict(dictionary:dict):
   print([dictionary[x] for x in sorted(dictionary.keys())])
+
+def div_k(rep,k = 2):
+  sum = 0
+  for i in range(len(rep)):
+    for j in range(i+1,len(rep)):
+      if rep[i]/rep[j]>1 and rep[i]/rep[j]<=k:
+        sum += 1
+  for i in range(len(rep)-1):
+    if rep[i]/rep[i+1]>k:
+      sum+=i+1
+  return sum
+
+def stat_even(rep,k = 2):
+  sum = 0
+  for i in range(len(rep)):
+    for j in range(i+1,len(rep)):
+      if rep[i]>rep[j] and rep[j]%2==1:
+        sum += 1
+  for i in range(len(rep)-1):
+    if rep[i]>rep[i+1] and rep[i+1]%2==0:
+      sum+=i+1
+  return sum
+
+def magic_stat_even(rep):
+  sum = 0
+  for i in range(len(rep)):
+    for j in range(i+1,len(rep)):
+      if rep[i]<rep[j] and rep[i]%2==1:
+        sum += 1
+  for i in range(len(rep)-1):
+    if rep[i]<rep[i+1] and rep[i]%2==0:
+      sum+=len(rep)-(i+1)
+  return sum
+
+def magic_div_k(rep,k = 2):
+  sum = 0
+  for i in range(len(rep)):
+    for j in range(i+1,len(rep)):
+      if rep[i]/rep[j]<1 and rep[i]/rep[j]>=1/k:
+        sum += 1
+  for i in range(len(rep)-1):
+    if rep[i]/rep[i+1]<1/k:
+      sum+=len(rep)-(i+1)
+  return sum
+
+
+
+
+
